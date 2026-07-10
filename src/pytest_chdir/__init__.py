bf132e279b2662f72318143ef6c4417f7e5c0489
Reuben Thomas <rrt@sc3d.org>
2026-07-10 20:36:47 +0100

    Update dependencies and code for 2026
    + Set maintainer to myself
    + Bump version to 0.2.0 (no real change, but denote update and new
      maintainer)
    + Update Python to minimum 3.11
    + Update dependencies to newest, except for pytest, as pytest-black does not
      work with pytest ≥ 9.1
    + Update from tool.poetry.dev-dependencies to
      tool.poetry.group.dev.dependencies
    + Use importlib.metadata instead of defunct pkg_resources

--- src/pytest_chdir/__init__.py
+++ src/pytest_chdir/__init__.py
@@ -1,8 +1,8 @@
-import pkg_resources
+import importlib.metadata
 
 from .plugin import define_chdir_fixture
 
 try:
-    __version__ = pkg_resources.get_distribution("pytest-chdir").version
-except pkg_resources.DistributionNotFound:
+    __version__ = importlib.metadata.version("pytest-chdir")
+except importlib.metadata.PackageNotFoundError:
     __version__ = "develop"
